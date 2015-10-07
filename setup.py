c18f7ef641f5987ec3e9132080f984d671a7578c
Simone Accascina <simon@accascina.me>
2015-10-07 23:45:21 +0200

    Version bump to 1.0.2

--- setup.py
+++ setup.py
@@ -1,30 +1,8 @@
-import os
-import sys
-from distutils.sysconfig import get_python_lib
-
 from setuptools import find_packages, setup
-
-# Warn if we are installing over top of an existing installation. This can
-# cause issues where files that were deleted from a more recent Django are
-# still present in site-packages. See #18115.
-overlay_warning = False
-if "install" in sys.argv:
-    lib_paths = [get_python_lib()]
-    if lib_paths[0].startswith("/usr/lib/"):
-        # We have to try also with an explicit prefix of /usr/local in order to
-        # catch Debian's custom user site-packages directory.
-        lib_paths.append(get_python_lib(prefix="/usr/local"))
-    for lib_path in lib_paths:
-        existing_path = os.path.abspath(os.path.join(lib_path, "django"))
-        if os.path.exists(existing_path):
-            # We note the need for the warning here, but present it after the
-            # command is run, so it's more likely to be seen.
-            overlay_warning = True
-            break
 
 setup(
     name='TelegramLogHandler',
-    version='1.0.0',
+    version='1.0.2',
     url='https://github.com/simonacca/TelegramLogHandler',
     author='Simone Accascina',
     author_email='simon@accascina.me',
